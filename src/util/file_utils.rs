c3f3c56bcfce729812a32afc2887f56bb4f84e8d
Kawaxte <31629699+Kawaxte@users.noreply.github.com>
2023-11-13 08:27:58 +0200

    edit: refactor `select_dir()` fn

--- src/util/file_utils.rs
+++ src/util/file_utils.rs
@@ -15,15 +15,13 @@
 
 use rfd::FileDialog;
 
-pub fn read_contents(path: std::path::PathBuf) -> std::io::Result<String> {
+pub fn read_contents_of(path: std::path::PathBuf) -> std::io::Result<String> {
     let contents = std::fs::read_to_string(path.to_owned())
         .expect(format!("Failed to read contents of '{:?}'", path).as_str());
     Ok(contents)
 }
 
-pub fn open_file_dialog() -> std::path::PathBuf {
-    println!("Opening file dialog... (1 of 2)");
-
+pub fn select_text_file() -> std::path::PathBuf {
     let src = match FileDialog::new()
         .add_filter("TEXT (*.txt)", &["txt"])
         .pick_file()
@@ -33,17 +31,23 @@ pub fn open_file_dialog() -> std::path::PathBuf {
             return std::path::PathBuf::new();
         }
     };
+
+    println!("Reading URL(s) from '{}'", src.display());
     src
 }
 
-pub fn open_file_dialog_for_dir() -> std::path::PathBuf {
-    println!("Opening file dialog... (2 of 2)");
-
-    let dest = match FileDialog::new().pick_folder() {
-        Some(dir) => dir,
-        None => {
-            return std::path::PathBuf::new();
+pub fn select_dir() -> std::path::PathBuf {
+    let dest = if let Some(dir) = FileDialog::new().pick_folder() {
+        dir
+    } else {
+        let mut user_home = dirs::home_dir().expect("Failed to get user home directory");
+        user_home.push(".royalty");
+        if !user_home.exists() {
+            std::fs::create_dir(user_home.to_owned()).expect("Failed to create directory");
         }
+        user_home
     };
+
+    println!("Downloading file(s) to '{}'", dest.display());
     dest
 }
